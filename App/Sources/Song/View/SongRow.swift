10611661260d9e47e375c3c3daf1e3bda12483b9
Luiz Fernando Salvaterra <90700402+LuizFernandoSalvaterra@users.noreply.github.com>
2025-08-13 16:35:40 -0300

    adds new async image loading for song row view

--- App/Sources/Song/View/SongRow.swift
+++ App/Sources/Song/View/SongRow.swift
@@ -1,25 +1,33 @@
 import SwiftUI
+import Data
 
 struct SongRow: View {
 
-    let title: String
-    let artist: String
+    let track: Track
 
     var body: some View {
         HStack(spacing: 16) {
-            RoundedRectangle(cornerRadius: 8)
-                .fill(Color(.white).opacity(0.1))
-                .frame(width: 44, height: 44)
-                .overlay(
-                    Image(.iconMusicNote)
-                        .foregroundColor(.white)
-                        .font(.title2)
-                )
+            AsyncImage(url: track.artworkURL) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+            } placeholder: {
+                RoundedRectangle(cornerRadius: 8)
+                    .fill(Color(.white).opacity(0.1))
+                    .overlay(
+                        Image(.iconMusicNote)
+                            .foregroundColor(.white)
+                            .font(.title2)
+                    )
+            }
+            .frame(width: 44, height: 44)
+            .clipShape(RoundedRectangle(cornerRadius: 8))
+            
             VStack(alignment: .leading, spacing: 4) {
-                Text(title)
+                Text(track.title)
                     .foregroundColor(Color(.white))
                     .font(.system(size: 16, weight: .regular))
-                Text(artist)
+                Text(track.artist)
                     .foregroundColor(Color(.gray500))
                     .font(.system(size: 12, weight: .regular))
             }
@@ -30,11 +38,11 @@ struct SongRow: View {
 }
 
 #Preview("Light") {
-    SongRow(title: "Something", artist: "Artist")
+    SongRow(track: .preview)
         .preferredColorScheme(.light)
 }
 
 #Preview("Dark") {
-    SongRow(title: "Something", artist: "Artist")
+    SongRow(track: .preview)
         .preferredColorScheme(.dark)
 }
