7a7a3f56b7e832defb839daf5c9005bb74f2beb0
Luiz Fernando Salvaterra <90700402+LuizFernandoSalvaterra@users.noreply.github.com>
2025-08-13 16:35:50 -0300

    chore: update localizable strings

--- App/Sources/Generated/LocalizedStrings.swift
+++ App/Sources/Generated/LocalizedStrings.swift
@@ -2,6 +2,8 @@ import Foundation
 
 enum LocalizedStrings {
     static let noInternetConnection = NSLocalizedString("no_internet_connection", comment: "")
+    static let noArtistSong = NSLocalizedString("no_artist_song", comment: "")
+    static let noTitleSong = NSLocalizedString("no_title_song", comment: "")
     static let openAlbum = NSLocalizedString("open_album", comment: "")
     static let search = NSLocalizedString("search", comment: "")
     static let songs = NSLocalizedString("songs", comment: "")
